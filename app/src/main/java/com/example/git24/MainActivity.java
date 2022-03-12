a422511429f5964425a106a2bcc34db904d1e1e4
Chumochka <danboez2004@bk.ru>
2022-03-12 14:57:49 +0700

    Добавление коментария commit5

--- app/src/main/java/com/example/git24/MainActivity.java
+++ app/src/main/java/com/example/git24/MainActivity.java
@@ -7,7 +7,7 @@ import android.os.Bundle;
 public class MainActivity extends AppCompatActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {//commit5
         super.onCreate(savedInstanceState);//commit3
         setContentView(R.layout.activity_main);//commit1
     }
